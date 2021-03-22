f2cf7604801d03e71ef8a0446c52f9fdc7471e93
Shahria Jaman Khan <41187226+shahria7k@users.noreply.github.com>
2021-03-22 21:10:36 +0600

    navigate

--- src/components/Login.js
+++ src/components/Login.js
@@ -59,7 +59,7 @@ export default function Login() {
 			setError("");
 			setLoading(true);
 			await login(emailRef.current.value, passwordRef.current.value);
-			history.push("/dashboard");
+			history.push("/navigate");
 		} catch {
 			setError("Failed to log in");
 		}
